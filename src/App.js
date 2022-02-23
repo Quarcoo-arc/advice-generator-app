1ae5df9a2c4f04ff6e6047575272d3d0e70d1dc7
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-23 19:26:58 +0000

    Footer component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Advice from "./components/Advice";
+import Footer from "./components/Footer";
 import { AdviceContextProvider } from "./context/AdviceContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -6,8 +7,9 @@ import "react-toastify/dist/ReactToastify.min.css";
 function App() {
   return (
     <AdviceContextProvider>
-      <div>
+      <div className="container">
         <Advice />
+        <Footer />
       </div>
       <ToastContainer />
     </AdviceContextProvider>
